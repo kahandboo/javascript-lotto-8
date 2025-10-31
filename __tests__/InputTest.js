cc374efa3832522cfdcba3bdf3d4f8b612a98922
감사감 <kokio02@pusan.ac.kr>
2025-10-31 13:38:23 +0900

    feat: 당첨 번호 입력 기능 구현

---
+++ __tests__/InputTest.js
@@ -0,0 +1,41 @@
+import Input from '../src/Input';
+import { INPUT_MESSAGES } from '../src/Constants';
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+describe("Input 클래스 테스트", () => {
+  let input;
+    
+  beforeEach(() => {
+    input = new Input();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  })
+
+  test("getPurchaseInput 호출 시 INPUT_MESSAGES.PURCHASE를 출력하고 사용자 입력을 반환한다", async () => {
+    // given
+    const mockReturnValue = "1000"; 
+    jest.spyOn(MissionUtils.Console, "readLineAsync").mockResolvedValue(mockReturnValue);
+    
+    // when
+    const result = await input.getPurchaseInput();
+    
+    // then
+    expect(MissionUtils.Console.readLineAsync).toHaveBeenCalledWith(INPUT_MESSAGES.PURCHASE);
+    expect(result).toBe(mockReturnValue);
+  });
+
+  test("getLottoNumbersInput 호출 시 INPUT_MESSAGES.LOTTO_NUMBERS를 출력하고 사용자 입력을 반환한다", async () => {
+    // given
+    const mockReturnValue = "1000"; 
+    jest.spyOn(MissionUtils.Console, "readLineAsync").mockResolvedValue(mockReturnValue);
+    
+    // when
+    const result = await input.getLottoNumbersInput();
+    
+    // then
+    expect(MissionUtils.Console.readLineAsync).toHaveBeenCalledWith(INPUT_MESSAGES.LOTTO_NUMBERS);
+    expect(result).toBe(mockReturnValue);
+  });
+});
--- src/Input.js
+++ src/Input.js
@@ -2,9 +2,13 @@ import { INPUT_MESSAGES } from "./Constants";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class Input {
-    getPurchaseInput() {
-        return MissionUtils.Console.readLineAsync(INPUT_MESSAGES.PURCHASE);
-    }
+  getPurchaseInput() {
+    return MissionUtils.Console.readLineAsync(INPUT_MESSAGES.PURCHASE);
+  }
+
+  getLottoNumbersInput() {
+    return MissionUtils.Console.readLineAsync(INPUT_MESSAGES.LOTTO_NUMBERS);
+  }
 }
 
 export default Input;
