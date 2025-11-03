f3727639d6c2abb40877c074a8cd04b7d6af1226
감사감 <kokio02@pusan.ac.kr>
2025-11-03 15:44:54 +0900

    refactor: 순위 판별로직을 LottoGame에서 Lotto로 이동

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,4 +1,4 @@
-import { ERROR_MESSAGES } from "../utils/Constants.js";
+import { ERROR_MESSAGES, RANK } from "../utils/Constants.js";
 
 class Lotto {
   #numbers;
@@ -27,6 +27,30 @@ class Lotto {
         throw new Error(ERROR_MESSAGES.LOTTO_NUMBER_OUT_OF_RANGE);
       }
     });
+  }
+
+  #countMatches(inputLotto) {
+    const inputNumbers = inputLotto.numbers; 
+    return this.#numbers.filter(num => inputNumbers.includes(num)).length;
+  }
+
+  #hasBonus(bonus) {
+    const bonusNumber = bonus.number;
+    return this.#numbers.includes(bonusNumber);
+  }
+
+  determineRank(inputLotto, bonus) {
+    const matchCount = this.#countMatches(inputLotto);
+    const hasBonus = this.#hasBonus(bonus);
+
+    if (matchCount === RANK.FIRST.match) return RANK.FIRST;
+    if (matchCount === RANK.SECOND.match) {
+      if (hasBonus) return RANK.SECOND;
+      else return RANK.THIRD;
+    }
+    if (matchCount === RANK.FOURTH.match) return RANK.FOURTH;
+    if (matchCount === RANK.FIFTH.match) return RANK.FIFTH;
+    return RANK.NONE;
   }
 }
 
--- src/domain/LottoGame.js
+++ src/domain/LottoGame.js
@@ -1,5 +1,3 @@
-import { RANK } from "../utils/Constants.js";
-
 class LottoGame {
   constructor(purchasedLottos, inputLotto, bonus) {
     this.purchasedLottos = purchasedLottos; 
@@ -7,21 +5,10 @@ class LottoGame {
     this.bonus = bonus;  
   }
 
-  #determineRank(purchasedLotto) {
-    const matchCount = purchasedLotto.filter(num => this.inputLotto.includes(num)).length;
-    const hasBonus = purchasedLotto.includes(this.bonus);
-
-    if (matchCount === RANK.FIRST.match) return RANK.FIRST;
-    if (matchCount === RANK.SECOND.match) {
-      if (hasBonus) return RANK.SECOND;
-      else return RANK.THIRD;
-    }
-    if (matchCount === RANK.FOURTH.match) return RANK.FOURTH;
-    if (matchCount === RANK.FIFTH.match) return RANK.FIFTH;
-  }
-
   getAllRanks() {
-    return this.purchasedLottos.map(purchasedLotto => this.#determineRank(purchasedLotto));
+    return this.purchasedLottos.map(purchasedLotto =>
+      purchasedLotto.determineRank(this.inputLotto, this.bonus)
+    );
   }
 }
 
