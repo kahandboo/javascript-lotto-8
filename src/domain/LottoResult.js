3e9e057405cea833f2e9f5c24163cf418f2dcb8a
감사감 <kokio02@pusan.ac.kr>
2025-11-03 14:43:23 +0900

    fix(LottoResult): 수익률 계산 시 NaN 반환 버그 수정

--- src/domain/LottoResult.js
+++ src/domain/LottoResult.js
@@ -5,6 +5,7 @@ class LottoResult {
 
   constructor() {
     this.#rankCounts = {
+      [RANK.NONE.key]: 0,
       [RANK.FIRST.key]: 0,
       [RANK.SECOND.key]: 0,
       [RANK.THIRD.key]: 0,
@@ -27,7 +28,8 @@ class LottoResult {
     const totalPrize = Object.values(RANK).reduce((sum, rank) => {
       return sum + (this.#rankCounts[rank.key] || 0) * rank.prize;
     }, 0);
-      
+    
+    if (totalPrize === 0) return 0;
     return (totalPrize / purchaseAmount) * 100;
   }
 }
